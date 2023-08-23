9a9fec2276cc703ee4595963563d5419f30b1b5e
utbytesstudent <zacharina.sebek@gmail.com>
2023-08-23 22:01:42 +0200

    groda

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+const cursorButton = document.getElementById('cursorButton');
+
+cursorButton.addEventListener('click', () => {
+  document.body.classList.toggle('custom-cursor');
+});
+
